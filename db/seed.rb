cf1531d2935a19016ae36aca4306928e1897f9a1
pavanreddy21 <pavanreddygottimukkula@gmail.com>
2023-09-25 00:37:20 -0400

    fix seed

--- db/seed.rb
+++
@@ -1,4 +0,0 @@
-# db/seeds.rb
-
-# Create a new user
-User.create(name: "John Doe", address: "123 Main St", email: "john.doe@example.com", phone: "555-555-5555")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Create a new user
+User.create(name: "John Doe", address: "123 Main St", email: "john.doe@example.com", phone: "555-555-5555")
